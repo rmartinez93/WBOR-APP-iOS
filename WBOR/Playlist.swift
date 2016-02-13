ea673818c3e4423cd864a7fb51832d0594bb7e16
Ruben Martinez Jr <ruben.martinez93@gmail.com>
2016-02-13 13:37:06 -0800

    Record Animation improvements

--- WBOR/Playlist.swift
+++ WBOR/Playlist.swift
@@ -13,6 +13,7 @@ class Playlist : NSObject {
     var curSong   : String?
     var curArtist : String?
     var curShow   : String?
+    var lastData  : NSData?
     
     init(url: NSURL) {
         self.wborInfo = url
@@ -21,20 +22,26 @@ class Playlist : NSObject {
     
     func getCurrent(callback: () -> ()) {
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
-            let data  = NSData(contentsOfURL: self.wborInfo)
+            let playlistData  = NSData(contentsOfURL: self.wborInfo)
             
-            do {
-                let json  = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? [String: String]
-                
-                if let info = json {
-                    self.curSong    = info["song_string"]!
-                    self.curArtist  = info["artist_string"]!
-                    self.curShow    = info["program_title"]!
+            if let data = playlistData ?? self.lastData {
+                dispatch_async(dispatch_get_main_queue()) {
+                    do {
+                        let json  = try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [String: String]
+                        
+                        if let info = json {
+                            self.curSong    = info["song_string"]!
+                            self.curArtist  = info["artist_string"]!
+                            self.curShow    = info["program_title"]!
+                        }
+                        
+                        self.lastData = data
+                        
+                        callback()
+                    }  catch _ {
+                        print("Bad JSON Data received: See Playlist Class.");
+                    }
                 }
-                
-                dispatch_async(dispatch_get_main_queue(), callback)
-            } catch _ {
-                print("Bad JSON Data received: See Playlist Class.");
             }
         }
     }
--- WBOR/RootViewController.swift
+++ WBOR/RootViewController.swift
@@ -12,10 +12,10 @@ import AVFoundation
 import AudioToolbox
 import QuartzCore
 
-class RootViewController : UIViewController {
+class RootViewController : UIViewController, UIGestureRecognizerDelegate {
     @IBOutlet var current : UILabel!
     @IBOutlet var currentArtist : UILabel!
-    @IBOutlet var record : UIImageView!
+    @IBOutlet var record : RMShapedImageView!
     
     var wborURL     = NSURL(string: "http://139.140.232.18:8000/WBOR")
     var playlistURL = NSURL(string: "http://wbor-hr.appspot.com/updateinfo")
@@ -36,11 +36,59 @@ class RootViewController : UIViewController {
         } catch _ {
         }
         
+        
+        super.viewDidLoad()
+
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "audioPlayerInterrupted:", name: AVAudioSessionInterruptionNotification, object: nil)
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "togglePlay", name: "playButtonTapped", object: nil)
+    }
+    
+    func rotationDetected(rotationGesture: UIRotationGestureRecognizer) {
+        if rotationGesture.state == UIGestureRecognizerState.Began || rotationGesture.state == UIGestureRecognizerState.Changed {
+            let rotation = rotationGesture.rotation
+            let currentAngle = CGFloat((record.layer.valueForKeyPath("transform.rotation.z")?.floatValue)!)
+            record.layer.setValue(currentAngle + (rotation/10), forKeyPath: "transform.rotation.z")
+        }
+    }
+    
+    var recordInterrupted: Bool = false
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if self.playing {
+            touches.forEach { (touch) -> () in
+                let touchPoint = touch.locationInView(self.view)
+                let imagePoint = record.convertPoint(touchPoint, fromView: self.view)
+                if self.playing && record.pointInside(imagePoint, withEvent: event) {
+                    self.stopRecordAnimation()
+                    self.recordInterrupted = true
+                }
+            }
+        }
+    }
+    
+    var lastAngle: CGFloat?
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        touches.forEach { (touch) -> () in
+            let touchPoint = touch.locationInView(self.view)
+            let imagePoint = record.convertPoint(touchPoint, fromView: self.view)
+            if record.pointInside(imagePoint, withEvent: event) {
+                let angle = atan2(touchPoint.y-self.record.center.y, touchPoint.x-self.record.center.x)
+                
+                let position = CGFloat((record.layer.valueForKeyPath("transform.rotation.z")?.floatValue)!)
+                let nextPosition = position + (angle - (lastAngle ?? angle))
+                record.layer.setValue(nextPosition, forKeyPath: "transform.rotation.z")
+                
+                lastAngle = angle
+            }
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if self.playing && self.recordInterrupted {
+            self.startRecordAnimation()
+        }
         
-        super.viewDidLoad()
+        lastAngle = nil
     }
     
     deinit {
@@ -86,12 +134,12 @@ class RootViewController : UIViewController {
     //toggle UI for song/artist info
     func updateInfo(playing: Bool, buffering: Bool) {
         if playing {
-            current.text = "Buffering..."
-            current.hidden = false
-            currentArtist.hidden = true
             if self.update != nil {
                 self.update!.invalidate()
             }
+            current.text = "Buffering..."
+            current.hidden = false
+            currentArtist.hidden = true
             
             if buffering {
                 return //don't load playlist info if we're struggling
@@ -107,16 +155,20 @@ class RootViewController : UIViewController {
                 repeats: true)
         }
         else {
-            current.hidden = true
-            currentArtist.hidden = true
             if self.update != nil {
                 self.update!.invalidate()
             }
+            current.hidden = true
+            currentArtist.hidden = true
         }
     }
     
     //update UI for song/artist info
     func displayPlaylistInfo() {
+        if !self.playing {
+            return //make sure we don't show anything when not playing
+        }
+        
         if self.playlist == nil {
             //initialize playlist if not already initialized
             self.playlist = Playlist(url: playlistURL!)
@@ -144,19 +196,27 @@ class RootViewController : UIViewController {
     //toggle record animation
     func updateRecordState(playing: Bool) {
         if playing {
-            let rotation = CABasicAnimation(keyPath: "transform.rotation")
-            let currentAngle = record.layer.valueForKeyPath("transform.rotation.z")?.doubleValue
-            rotation.fromValue = NSNumber(double: currentAngle!)
-            rotation.toValue = NSNumber(double: 2*M_PI + currentAngle!)
-            rotation.duration = 2
-            rotation.repeatCount = Float.infinity
-            rotation.removedOnCompletion = false
-            rotation.fillMode = kCAFillModeForwards
-            record.layer.addAnimation(rotation, forKey: "spin")
+            startRecordAnimation()
         } else {
-            record.layer.transform = record.layer.presentationLayer()!.transform
-            record.layer.removeAnimationForKey("spin")
+            stopRecordAnimation()
         }
+    }
+    
+    func startRecordAnimation() {
+        let rotation = CABasicAnimation(keyPath: "transform.rotation")
+        let currentAngle = record.layer.valueForKeyPath("transform.rotation.z")?.doubleValue
+        rotation.fromValue = NSNumber(double: currentAngle!)
+        rotation.toValue = NSNumber(double: 2*M_PI + currentAngle!)
+        rotation.duration = 2
+        rotation.repeatCount = Float.infinity
+        rotation.removedOnCompletion = false
+        rotation.fillMode = kCAFillModeForwards
+        record.layer.addAnimation(rotation, forKey: "spin")
+    }
+    
+    func stopRecordAnimation() {
+        record.layer.transform = record.layer.presentationLayer()!.transform
+        record.layer.removeAnimationForKey("spin")
     }
     
     //toggle AVPlayer stream
